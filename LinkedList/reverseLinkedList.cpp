12d92ea84aaa222cbbc5f9d1b5f880252e9fb34c
Shhrinika1115 <168533949+Shhrinika1115@users.noreply.github.com>
2026-06-21 12:18:02 +0530

    reverseLinkedList.cpp

--- LinkedList/reverseLinkedList.cpp
+++ LinkedList/reverseLinkedList.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+using namespace std;
 
+// Definition of linked list node
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+};
+
+// Function to print linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original Linked List: ";
+    printList(head);
+
+    Solution s;
+
+    // Reverse the linked list
+    ListNode* reversedHead = s.reverseList(head);
+
+    cout << "Reversed Linked List: ";
+    printList(reversedHead);
+
+    return 0;
+}
